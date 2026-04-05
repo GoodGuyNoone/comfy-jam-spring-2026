3157a438891f7535b99d7d5a005b69082c2e10b9
Aleksandr.Churakov <aleksandr.churakov@fooda.com>
2026-04-05 16:10:04 +0200

    small fix

--- scripts/flower.gd
+++ scripts/flower.gd
@@ -10,7 +10,7 @@ signal picked(flower_id)
 func _ready() -> void:
     sprite.texture = texture
 
-func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+func _input_event(viewport, event, shape_idx) -> void:
     if event is InputEventMouseButton and event.pressed and event.button_index ==MOUSE_BUTTON_LEFT:
         picked.emit(flower_id)
-        print($"emits flower_id: ", flower_id)
+        print("emits flower_id: %s" % flower_id)
